a40f719fd98ab19f4c0ebe04a246c8f30655b785
BenjaminGWilson <benjamin.g.wilson@gmail.com>
2021-10-04 16:22:13 +0100

    updating so search string is programatic

--- get_lesbian_tweets_as_csv.py
+++ get_lesbian_tweets_as_csv.py
@@ -1,23 +1,32 @@
-import snscrape.modules.twitter as sntwitter
-import pandas as pd
+# import snscrape.modules.twitter as sntwitter
+# import pandas as pd
 from datetime import date, timedelta
 
-# Creating list to append tweet data to
-tweets_list2 = []
+# # Creating list to append tweet data to
+# tweets_list2 = []
 
 # Using TwitterSearchScraper to scrape data and append tweets to list
 search_term = "lesbian"
-one_hour_ago = date.today - timedelta(1)
+negative_terms = "-xxx -porn -erotic"
+
+def yesterday():
+    day = timedelta(days=1)
+    today = date.today()
+    yesterday = today - day
+    return yesterday
+
+string= f"{search_term} {negative_terms} since:{yesterday()}"
+print(string)
 
 # todo
 # use time delta to make string that searches from yesterday
 # add string to line below this
-for i,tweet in enumerate(sntwitter.TwitterSearchScraper('lesbian since').get_items()):
-    if i>500:
-        break
-    tweets_list2.append([tweet.date, tweet.id, tweet.content, tweet.user.username])
+# for i,tweet in enumerate(sntwitter.TwitterSearchScraper('lesbian since').get_items()):
+#     if i>500:
+#         break
+#     tweets_list2.append([tweet.date, tweet.id, tweet.content, tweet.user.username])
     
-# Creating a dataframe from the tweets list above
-tweets_df2 = pd.DataFrame(tweets_list2, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
+# # Creating a dataframe from the tweets list above
+# tweets_df2 = pd.DataFrame(tweets_list2, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
 
-tweets_df2.to_csv("lesbiantweets.csv")
+# tweets_df2.to_csv("lesbiantweets.csv")
