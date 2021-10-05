1aad229f54671b76ff5f970141bc7fd9657d99ab
BenjaminGordonCody <b.gordoncody@gmail.com>
2021-10-05 15:39:57 +0100

    improved comments

--- text_dataframe_utils.py
+++ text_dataframe_utils.py
@@ -11,14 +11,15 @@ import os
 
 
 def multiple_csv_to_single_dataframe(folder):
-    """Takes the name of a folder that contains CSV, combines .csv contents
-    to single df"""
+    """Takes the name of a folder as a string. Combines .csv contents to
+    single df. Using raw string (ie prefixing string wtih 'r') can solve
+    Unicode issues"""
     joined_files = os.path.join(folder, "*.csv")
     globfiles = glob.glob(joined_files)
     df = pd.concat(map(pd.read_csv, globfiles), ignore_index=True)
-    print(df)
+    return df
 
 
 if __name__ == "__main__":
-    multiple_csv_to_single_dataframe(
+    df = multiple_csv_to_single_dataframe(
         r"C:\Users\Benjamin\Documents\GitHub\Novel-Lesbian-Category-Finder")
